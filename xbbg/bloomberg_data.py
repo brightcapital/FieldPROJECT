4148f5bf0842f25e5e76f4908819ae4c34bfb3ba
Shradha Maria <shradhamaria@Shradhas-MacBook-Pro.local>
2023-10-24 15:12:54 +0200

    updated  code

--- xbbg/bloomberg_data.py
+++ xbbg/bloomberg_data.py
@@ -3,38 +3,27 @@ import pandas as pd
 from xbbg import blp
 
 
-def fetch_price_from_bloomberg(ticker_symbols, start_date, end_date):
-    price = blp.bdh(tickers=ticker_symbols, flds=['last_price'],start_date=start_date, end_date=end_date, Per='M')
-    price.dropna(inplace=True, thresh=3,axis=0)
+def fetch_price_from_bloomberg(ticker_symbols, equities, currency, start_date, end_date):
+    price_data = blp.bdh(tickers=ticker_symbols, flds=['last_price'],start_date=start_date, end_date=end_date, Per='M')
+    price_data.dropna(inplace=True, thresh=3,axis=0)
+    price_data = price_data.drop(price_data.index[2])
+    price_data = price_data.rename(columns={'Unnamed: 0': 'BBG Ticker'})
+    price_data.insert(0, "Equities", equities)
+    price_data.insert(1, "Currency", currency)
 
     # Save the data to a CSV file
-    price.to_csv('bloomberg_price.csv')
+    price_data.to_csv('bloomberg_price.csv')
 
-
-def fetch_currency_from_bloomberg(ticker_symbols):
-    currency = blp.bdp(tickers=ticker_symbols, flds=['CRNCY'])
-    currency = currency.T
-    if ticker_symbols == "XAG CURNCY" or "XAU CURNCY":
-        currency["XAG CURNCY"] = "USD"
-        currency["XAU CURNCY"] = "USD"
-
-
-    # Save the data to a CSV file
-    currency.to_csv('bloomberg_curr.csv')
-
-
-def merge_price_currency():
-    price_data = pd.read_csv('bloomberg_price.csv')
-    currency_data = pd.read_csv('bloomberg_curr.csv')
-
-    data_combined = pd.concat([currency_data,price_data],axis=0)
-
-    data_combined.to_csv('bloomberg_data.csv',index=False)
 
 if __name__ == "__main__":
-    # Read ticker symbols from the CSV file
-    ticker_symbols_df = pd.read_csv('tickersymbols.csv')
-    ticker_symbols = sorted(ticker_symbols_df['Ticker'].tolist())
+    # Read ticker symbols, equities and currency from the CSV file
+    bloomberg_equities = pd.read_csv('tickers_equities.csv', index_col=False)
+    bloomberg_equities = bloomberg_equities.sort_values(by=['BBG Ticker'], ignore_index=True)
+
+    ticker_symbols = bloomberg_equities['BBG Ticker']
+    equities = bloomberg_equities['Equities']
+    currency = bloomberg_equities['Currency']
+
 
     start_date_str = input("Enter the start date (YYYY-MM-DD): ")
     start_date = datetime.strptime(start_date_str, '%Y-%m-%d')
@@ -42,6 +31,4 @@ if __name__ == "__main__":
     # Determine today's date
     end_date = datetime.today()
 
-    fetch_price_from_bloomberg(ticker_symbols, start_date, end_date)
-    fetch_currency_from_bloomberg(ticker_symbols)
-    merge_price_currency()
+    fetch_price_from_bloomberg(ticker_symbols, equities, currency, start_date, end_date)
